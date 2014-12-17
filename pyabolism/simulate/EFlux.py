86049f0d7977f9559040068e2abdfe76f05bcb62
Nick Fyson <nick@nickfyson.co.uk>
2014-12-17 14:26:41 +0000

    Clean up EFlux code and move helper functions into 'tools' module
    Functions to get transports and exchanges are moved into tools.py, and code in EFlux is commented and general clarified.

--- pyabolism/simulate/EFlux.py
+++ pyabolism/simulate/EFlux.py
@@ -3,87 +3,74 @@ import os,sys
 
 import numpy as np
 
-from .LP import grb, GRB, generate_basic_lp
-
 from .FBA import FBA
 
-import re
-import networkx as nx
+from ..tools import get_transport_reactions, get_exchange_reactions
+
+
 def _get_capacity(gene_association,expressions):
     """find the upper bound on a reaction with the given gene_association string"""
+    
     gene_association = gene_association.replace('(',' ').replace(')',' ')
+    
+    # we refer to each set of genes sufficient to catalyse the reaction as a 'clause'
     clauses = gene_association.lower().split(' or ')
     
     capacity = 0
     for clause in clauses:
+        # within each clause are a number of genes
         genes = [gene.strip() for gene in clause.split(' and ')]
-
+        # for each of these, the contribution is limited by the lowest capacity
         capacity += np.min([expressions.get(gene,np.infty) for gene in genes])
 
     return capacity
 
-def _get_exchange_reactions(model):
-    """exchange reactions are those that convert boundary metabolites into external metabolites"""
-    exchanges = []
-    
-    for r in model.reactions.values():
 
-        if [m for (m,s) in r.participants.items() if m.boundaryCondition]:
-            exchanges.append( r )
-            continue
-
-        if len(r.participants)==1:
-            exchanges.append( r )
-            continue
-
-    return exchanges
-
-def EFlux(model,expressions,limit_unpaired=False,norm='L2',show=False,alt_eflux=False):
+def EFlux(model,expressions,limit_unpaired=False,norm='L2',show=False):
     
-    exchanges = _get_exchange_reactions(model)
-    
-    # build list of all transport reactions (external to cytoplasm compartments)
-    transports = []
-    for r in model.reactions.values():
-        if set(['e','c'])==set([m.compartment for m in r.participants.keys()]):
-            transports.append( r )
-    
+    # we compile a dictionary of capacities for all reactions in the model
     capacities = {}
-
     for r in model.reactions.values():
         gene_association = r.notes.get('GENE_ASSOCIATION','')
 
         capacities[r.id] = _get_capacity(gene_association,expressions)
     
-    norm_capacity = np.median( [v for k,v in capacities.items() if v] )
-
+    # in situations where we limit reactions that are *not* limited by expression
+        # we store the median of all determined capacties
+    median_capacity = np.median( [v for k,v in capacities.items() if v] )
+    
+    # the bounds on each reaction are set in turn
     for r in model.reactions.values():
 
-        if r in exchanges or r in transports:
-            continue
-                
         capacity = capacities[r.id]
         
+        # if expression-inspired limits are scarce...
         if not capacity:
+            #...we may wish to cap unbounded reactions
             if limit_unpaired:
-                capacity = norm_capacity
+                capacity = median_capacity
+            #...otherwise, just leave it as unbounded
             else:
                 capacity = np.infty
         
+        # reversible reactions use the same enzyme, and hence get the same bounds
         if r.reversible:
             r.lower_bound = -capacity
+        # irreversible reactions cannot have negative flux under any circumstances
         else:
             r.lower_bound = 0.0
-
+        
         r.upper_bound = capacity
     
-    # make any activated exchanges unlimited
-    for r in exchanges:
-        if r.lower_bound < 0.0:
+    # make any activated exchanges and transports unlimited
+        # while respecting existing directionality of bounds
+    for r in get_exchange_reactions(model)+get_transport_reactions(model):
+        if r.lower_bound < 0.0 and r.reversible:
             r.lower_bound = -np.infty
         if r.upper_bound > 0.0:
             r.upper_bound = np.infty
-
+    
+    # with the reaction bounds appropriately set, the problem is just standard FBA
     FBA(model,show=show,norm=norm)
     
     return model
---
+++ pyabolism/tools.py
@@ -0,0 +1,26 @@
+
+def get_exchange_reactions(model):
+    """exchange reactions are those that convert boundary metabolites into external metabolites"""
+    exchanges = []
+    
+    for r in model.reactions.values():
+
+        if [m for (m,s) in r.participants.items() if m.boundaryCondition]:
+            exchanges.append( r )
+            continue
+
+        if len(r.participants)==1:
+            exchanges.append( r )
+            continue
+
+    return exchanges
+
+def get_transport_reactions(model):
+    """return all reactions that move metabolites across compartment boundaries"""
+    transports = []
+    for r in model.reactions.values():
+        if set(['e','c'])==set([m.compartment for m in r.participants.keys()]):
+            transports.append( r ) 
+    return transports
+
+
