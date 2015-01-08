ae75f00521a7b4ca046c27e0429cf9f1705e61e0
Nick Fyson <nick@nickfyson.co.uk>
2015-01-08 12:09:49 +0000

    Introduce example for changes in new release, and update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = "Pyabolism",
-    version = "0.1dev",
+    version = "0.2dev",
     author = "Nick Fyson",
     author_email = "nick@nickfyson.co.uk",
     description = ("A module for constraint-based simulation of metabolic networks."),
@@ -13,10 +13,9 @@ setup(
     packages=['pyabolism','pyabolism.simulate'],
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: BSD License",
         "Intended Audience :: Science/Research",
     ],
 )
-
