a0ffb7067636eb2b90509db78f0beee6f324d769
Nick Fyson <nick@nickfyson.co.uk>
2015-01-21 17:03:37 +0000

    Remove option to norm unpaired reactions, and add lots of comments

--- pyabolism/simulate/EFlux.py
+++ pyabolism/simulate/EFlux.py
@@ -26,64 +26,50 @@ def _get_capacity(gene_association,expressions):
     return capacity
 
 
-def EFlux(model,expressions,limit_unpaired='',norm='L2',show=False,unlimited_transports=False):
-
-    # we compile a dictionary of capacities for all reactions in the model
+def EFlux(model,expressions,norm='L2',show=False,unlimited_transports=False):
+    """implementation of the EF-Flux algorithm (Colijn et al)"""
+    
+    for r in model.reactions():
+        r.reset_bounds()
+ 
+    exchanges  = get_exchange_reactions(model)
+    transports = get_transport_reactions(model)
+    
+    # every reaction gets a maxiumum capacity, dictated by its GPR string
     capacities = {}
     for r in model.reactions():
         gene_association = r.notes.get('GENE_ASSOCIATION','')
 
         capacities[r.id] = _get_capacity(gene_association,expressions)
     
-    # in situations where we limit reactions that are *not* limited by expression
-        # we may wish to cap reaction flux in some other way
-    if limit_unpaired:
-        # limit all fluxes by the maximum of the determined bounds
-        if limit_unpaired=='maximum':
-            default_bound = np.max( [v for k,v in capacities.items() if v] )
-        # limit by median of determined bounds
-        elif limit_unpaired=='median':
-            default_bound = np.median( [v for k,v in capacities.items() if v] )
-        else:
-            sys.exit('Unknown value for limit_unpaired argument')
-    # if no limit type chosen, the maximum flux for unbounded reactions is infinite
-    else:
-        default_bound = np.infty
-    
-    # the bounds on each reaction are set in turn
     for r in model.reactions():
 
+        if r in exchanges:
+            continue
+        
+        if unlimited_transports and r in transports:
+            continue
+                
         capacity = capacities[r.id]
         
-        # if expression-inspired limits are not available in this case...
-        if not capacity:
-            # we use the default_bound determined earlier
-            capacity = default_bound
-        
-        # reversible reactions use the same enzyme, and hence get the same bounds
+        # this value for capacity determines the maximum flow possible through the reaction
+        r.upper_bound = capacity
+
+        # capacity due to enzyme expression can work in either direction (for reversible reactions)
         if r.reversible:
             r.lower_bound = -capacity
-        # irreversible reactions cannot have negative flux under any circumstances
         else:
             r.lower_bound = 0.0
-        
-        r.upper_bound = capacity
     
-    # make any activated exchanges and transports unlimited
-        # while respecting existing directionality of bounds
-    for r in get_exchange_reactions(model):
-        if r.lower_bound < 0.0 and r.reversible:
+    # growth medium may be set in a binary on/off way
+        # but we make any activated exchanges unlimited
+    for r in exchanges:
+        if r.lower_bound < 0.0:
             r.lower_bound = -np.infty
         if r.upper_bound > 0.0:
             r.upper_bound = np.infty
     
-    if unlimited_transports:
-        for r in get_transport_reactions(model):
-            if r.lower_bound < 0.0 and r.reversible:
-                r.lower_bound = -np.infty
-            if r.upper_bound > 0.0:
-                r.upper_bound = np.infty
-    # with the reaction bounds appropriately set, the problem is just standard FBA
+    # our problem can now be solved using the standard FBA algorithm
     FBA(model,show=show,norm=norm)
     
     return model
