78c48929fd57d556bd87d062db9c38f2f89f5c7e
Nick Fyson <nick@nickfyson.co.uk>
2015-06-26 17:18:43 +0100

    Bump version number for new release to 0.3

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 
 setup(
     name = "Pyabolism",
-    version = "0.2.3dev",
+    version = "0.3dev",
     author = "Nick Fyson",
     author_email = "nick@nickfyson.co.uk",
     description = ("A module for constraint-based simulation of metabolic networks."),
     license = "BSD",
     keywords = "FBA metabolic network",
-    url = "http://pypi.python.org/pypi/pyabolism",
+    #url = "http://pypi.python.org/pypi/pyabolism",
     packages=['pyabolism','pyabolism.simulate'],
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     classifiers=[
