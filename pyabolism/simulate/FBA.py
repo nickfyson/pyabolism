55bc3b5abc9eeb1a39a8b9b349eb49dce2297ff2
Nick Fyson <mail@nickfyson.co.uk>
2015-11-02 16:04:32 +0000

    Add prefix to variable and constraint names in Gurobi LP
    Previously variables and constraints were named using just the reaction and metabolite IDs,
    but for more sophisticated algorithms it is useful to have unique naming. For example, in
    situations where multiple models might be combined into a single LP. By default a random
    string is assigned to the ModelName attribute of the Gurobi model, and this is used as a prefix
    for all variable and constraint names.

--- pyabolism/simulate/FBA.py
+++ pyabolism/simulate/FBA.py
@@ -10,6 +10,8 @@ def FBA(model, show=False, norm=''):
     
     lp = generate_basic_lp(model)
 
+    prefix = lp.getAttr('ModelName') + '_'
+
     lp.optimize()
 
     if lp.status == GRB.status.INFEASIBLE:
@@ -23,12 +25,12 @@ def FBA(model, show=False, norm=''):
         # preserving the fluxes as found in the model before FBA
     for reaction in model.reactions():
         reaction.loaded_flux = reaction.flux_value
-        reaction.flux_value  = lp.getVarByName(reaction.id).X
+        reaction.flux_value  = lp.getVarByName(prefix + reaction.id).X
     
     # shadow price of a constraint is a property that can be useful for some analyses
     for metabolite in model.metabolites():
-        if lp.getConstrByName(metabolite.id):
-            metabolite.shadow = lp.getConstrByName(metabolite.id).getAttr('Pi')
+        if lp.getConstrByName(prefix + metabolite.id):
+            metabolite.shadow = lp.getConstrByName(prefix + metabolite.id).getAttr('Pi')
         else:
             metabolite.shadow = 0
     
@@ -43,7 +45,7 @@ def FBA(model, show=False, norm=''):
 
             for reaction in model.reactions():
                 
-                var        = lp.getVarByName(reaction.id)
+                var        = lp.getVarByName(prefix + reaction.id)
                 flux_value = reaction.flux_value
 
                 if reaction.objective_coefficient != 0:
@@ -69,7 +71,7 @@ def FBA(model, show=False, norm=''):
         elif norm == 'L2':
             objective = grb.QuadExpr()
             for reaction in model.reactions():
-                var        = lp.getVarByName(reaction.id)
+                var        = lp.getVarByName(prefix + reaction.id)
                 flux_value = reaction.flux_value
  
                 if reaction.objective_coefficient != 0:
@@ -93,7 +95,7 @@ def FBA(model, show=False, norm=''):
 
         # we store the new fluxes found thanks to the minimisation
         for reaction in model.reactions():
-            reaction.flux_value = lp.getVarByName(reaction.id).X
+            reaction.flux_value = lp.getVarByName(prefix + reaction.id).X
     
     # we store the total objective achieved as a property of the model
     model.total_objective = 0
--- pyabolism/simulate/LP.py
+++ pyabolism/simulate/LP.py
@@ -1,5 +1,7 @@
 
 import os
+import random
+import string
 
 import gurobipy as grb
 from gurobipy import GRB
@@ -12,12 +14,15 @@ except:
     pass
 
 
-def generate_basic_lp(model):
+def generate_basic_lp(model, lp=None):
     """docstring for _generate_basic_lp"""
     # we first initialise the linear program
-        
-    lp = grb.Model('fba')
-    
+    if lp:
+        prefix = lp.getAttr('ModelName') + '_'
+    else:
+        lp = grb.Model(''.join(random.choice(string.ascii_uppercase) for _ in range(5)))
+        prefix = lp.getAttr('ModelName') + '_'
+
     lp.modelSense = GRB.MAXIMIZE
     
     # reaction fluxes are the variables in the linear model
@@ -30,13 +35,13 @@ def generate_basic_lp(model):
                       reaction.upper_bound,
                       reaction.objective_coefficient,
                       GRB.CONTINUOUS,
-                      reaction.id)
+                      prefix + reaction.id)
         else:
             lp.addVar(max(reaction.lower_bound, 0.0),
                       reaction.upper_bound,
                       reaction.objective_coefficient,
                       GRB.CONTINUOUS,
-                      reaction.id)
+                      prefix + reaction.id)
     lp.update()
     
     # each metabolite becomes a constraint,
@@ -47,7 +52,7 @@ def generate_basic_lp(model):
         variables       = []
         stoichiometries = []
         for reaction in model.reaction.get_by_contains(metabolite):
-            variables.append(lp.getVarByName(reaction.id))
+            variables.append(lp.getVarByName(prefix + reaction.id))
             stoichiometries.append(reaction.participants[metabolite])
 
         if metabolite.boundaryCondition:
@@ -57,7 +62,7 @@ def generate_basic_lp(model):
         else:
             # the weighted sum of all the reactions featuring the metabolite
             # must have a net total of zero
-            lp.addConstr(grb.LinExpr(stoichiometries, variables), GRB.EQUAL, 0.0, metabolite.id)
+            lp.addConstr(grb.LinExpr(stoichiometries, variables), GRB.EQUAL, 0.0, prefix + metabolite.id)
     
     lp.update()
     
