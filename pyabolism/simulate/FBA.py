90b8bc0b9fec735e96809e17ab5e52f1053e9c32
Nick Fyson <nick@fyson.net>
2016-04-07 17:35:16 +0100

    Update FBA to use irreversible form when applying L1 norm

--- pyabolism/simulate/FBA.py
+++ pyabolism/simulate/FBA.py
@@ -1,12 +1,19 @@
 
+import re
 import numpy as np
 
 from .LP import grb, GRB, generate_basic_lp
+from .simtools import irreversify, deirreversify
 
 
 def FBA(model, show=False, norm=''):
     """builds and solves an FBA linear program,
        updating the flux_value property of each reaction accordingly"""
+
+    # if we are using the L1 norm, we must first convert the model
+    # into undirectional form
+    if norm == 'L1':
+        model = irreversify(model)
 
     generate_basic_lp(model)
 
@@ -32,6 +39,13 @@ def FBA(model, show=False, norm=''):
         else:
             metabolite.shadow = 0
 
+    # default behaviour is that *all* reactions are included in the
+    # minisation of overall norm
+    limiteds = model.reactions()
+
+    # limiteds = [r for r in model.reactions()
+    #             if re.findall(model.gene_regex, r.notes.get('GENE_ASSOCIATION', ''))]
+
     # in general the flux vector returned by FBA is not unique,
     # so optional minimization of the norm is offered
     if norm:
@@ -51,23 +65,19 @@ def FBA(model, show=False, norm=''):
                     # we slightly loosen bounds on the objective
                     var.lb = flux_value * (1. - 1e-8)
                     var.ub = np.infty
-                else:
-                    # if not part of the obective, we require only that the flux
-                    # remain of the same *sign* as found originally
-                    # to avoid numerical issues in floating point calculations,
-                    # we slightly loosen bounds on the objective
-                    var.lb = flux_value * (1. - 1e-6)
-                    var.lb = min(0, flux_value * (1. - 1e-8))
-                    var.ub = max(0, flux_value * (1. + 1e-8))
 
-                # all fluxes are in the objective function,
+                # if reaction is in the limited list
+                # then flux is in the objective function,
                 # such that we can minimise the *magnitudes*
-                objective += var * float(np.sign(flux_value))
+                if reaction in limiteds:
+                    objective += var
 
         # the euclidean norm requires non-linear objective,
         # but allows the sign of each component to vary freely
         elif norm == 'L2':
+
             objective = grb.QuadExpr()
+
             for reaction in model.reactions():
                 var        = reaction.lp_var
                 flux_value = reaction.flux_value
@@ -78,8 +88,10 @@ def FBA(model, show=False, norm=''):
                     var.lb = flux_value * (1. - 1e-6)
                     var.ub = np.infty
 
-                # the square of every flux is in the objective function
-                objective += var * var
+                # if reaction is in the limited list
+                # the square of the flux is in the objective function
+                if reaction in limiteds:
+                    objective += var * var
         else:
             raise Exception('Unknown norm type...')
 
@@ -95,10 +107,15 @@ def FBA(model, show=False, norm=''):
         for reaction in model.reactions():
             reaction.flux_value = reaction.lp_var.X
 
+    # if we used the L1 norm, we must now convert back to
+    # a model that permist reversible reactions
+    if norm == 'L1':
+        model = deirreversify(model)
+
     # we store the total objective achieved as a property of the model
     model.total_objective = 0
-    for reaction in \
-            [reaction for reaction in model.reactions() if reaction.objective_coefficient != 0]:
+    for reaction in [reaction for reaction in model.reactions()
+                     if reaction.objective_coefficient != 0]:
             model.total_objective += reaction.flux_value * reaction.objective_coefficient
 
             # where required, the flux through targeted reactions is output to the console
---
+++ pyabolism/simulate/simtools.py
@@ -0,0 +1,85 @@
+
+from copy import copy, deepcopy
+
+from ..model import Reaction
+
+
+def irreversify(original):
+
+    model = deepcopy(original)
+
+    model.original = original
+
+    # our approach requires that reversible reactions be treated separately,
+    # in forward and backward direction
+    for r in model.reactions():
+
+        # a new reaction is created
+        new_r = Reaction(r.id + '_f')
+
+        new_r.name                   = r.name
+        new_r.reversible             = False  # all reactions in our altered model are irreversible
+        new_r.lower_bound            = 0.0
+        new_r.upper_bound            = r.upper_bound
+        # all reactions in our altered model are irreversible
+        new_r.default_bounds         = (0.0, r.default_bounds[1])
+        new_r.objective_coefficient  = r.objective_coefficient
+        new_r.flux_value             = r.flux_value
+        new_r.notes                  = copy(r.notes)
+        new_r.genes                  = copy(r.genes)
+
+        new_r.participants           = copy(r.participants)
+
+        # for retrieving the eventual results, we require the following information
+        new_r.notes['original_rid']            = r.id
+        new_r.notes['original_rid_multiplier'] = 1.0
+
+        model.reaction.add(new_r)
+
+        # if the original reaction is reversible, we add a second (flipped) version to our new model
+        if r.reversible:
+            new_r = Reaction(r.id + '_b')
+
+            new_r.name                   = r.name
+            new_r.reversible             = False  # again, all reactions are irreversible
+            new_r.lower_bound            = 0.0
+            new_r.upper_bound            = abs(r.lower_bound)
+            # the upper bound is the absolute value of the original lower
+            new_r.default_bounds         = (0.0, abs(r.default_bounds[0]))
+            new_r.objective_coefficient  = r.objective_coefficient
+            new_r.flux_value             = r.flux_value
+            new_r.notes                  = copy(r.notes)
+            new_r.genes                  = copy(r.genes)
+
+            new_r.participants           = copy(r.participants)
+
+            new_r.notes['original_rid']            = r.id
+            new_r.notes['original_rid_multiplier'] = -1.0
+
+            # here we negate the stoichiometry of all participants
+            for m, s in new_r.participants.items():
+                new_r.participants[m] = -1.0 * s
+
+            model.reaction.add(new_r)
+
+        # now we have replaced the original reaciton with duplicate(s) we remove it
+        model.reaction.remove(r)
+
+    return model
+
+
+def deirreversify(model):
+
+    original = model.original
+
+    # we now sum the fluxes of each reaction in the model to the relevant reaction in original
+    # we first store the loaded value, and set the flux to zero
+    for r in original.reactions():
+        r.loaded_flux = r.flux_value
+        r.flux_value  = 0.0
+    # and then sum up the flux found in each associated reaction in the GC-Flux model
+    for r in model.reactions():
+        original.reaction[r.notes['original_rid']].flux_value += \
+            r.notes['original_rid_multiplier'] * r.flux_value
+
+    return original
